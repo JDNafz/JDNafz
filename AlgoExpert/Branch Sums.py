03fec09a446a5ceeaf9db01bb0f9f06bcb54835b
JD <nafziger10@gmail.com>
2022-08-16 15:19:29 -0500

    Adding my AlgoExpert Code

---
+++ AlgoExpert/Branch Sums.py
@@ -0,0 +1,44 @@
+'''
+https://www.algoexpert.io/questions/branch-sums
+
+'''
+
+
+
+
+# This is the class of the input root. Do not edit it.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+# branchSums is a function that returns a list
+def branchSums(root):
+    node = root
+    totals = []
+    if node.left is None and node.right is None:
+        #if it's a leaf, return node.value
+        return [node.value]
+    elif node.left is not None and node.right is not None:
+        # if it has two kids
+        totals = []
+        for element in branchSums(node.left):
+            totals.append( element + node.value)
+        for element in branchSums(node.right):
+            totals.append(element + node.value)
+        return totals
+    
+    elif node.left is not None:
+        print(r"""  """)
+        totals = []
+        for element in branchSums(node.left):
+            totals.append(element + node.value)
+        return totals
+    elif node.right is not None:
+        totals = []
+        for element in branchSums(node.right):
+            totals.append(element + node.value)
+        return totals
+    return totals
+ 
