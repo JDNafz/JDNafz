e77057b102d0f35999f7f1fd0fbd499794f18d2e
JD <nafziger10@gmail.com>
2022-08-18 08:52:40 -0500

    Update Max Subset Sum No Adjacent.py

--- AlgoExpert/Max Subset Sum No Adjacent.py
+++ AlgoExpert/Max Subset Sum No Adjacent.py
@@ -13,7 +13,6 @@ def maxSubsetSumNoAdjacent(array):
     # Write your code here.
     currentMax, previousMax = 0, 0
     for number in array:
-        
         previousMax, currentMax = currentMax, max(currentMax, previousMax + number)
     return currentMax
 
