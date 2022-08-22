eb978f56708960ad76212f29363a435924daecc4
JD <nafziger10@gmail.com>
2022-08-22 10:49:43 -0500

    Update Node Depths.py

--- AlgoExpert/Node Depths.py
+++ AlgoExpert/Node Depths.py
@@ -6,6 +6,22 @@ https://www.algoexpert.io/questions/node-depths
 '''
 def nodeDepths(root,depth = -1):
     # Write your code here.
+    '''most efficient code'''
+
+    if root is None:
+        return 0
+    return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth +1)
+    
+    
+    #if you run the function on root.left/right and nothing is there:
+        #return 0
+    #a child is inherently dept + 1 beyond what the current Node is
+
+
+
+'''
+
+    #FIRST METHOD I WROTE WITHOUT HELP:
     if root.left is None and root.right is None:
         depth +=1
         print(".   .Node",root.value,"depth:",depth)
@@ -20,7 +36,8 @@ def nodeDepths(root,depth = -1):
     else:
         depth += 1
         return depth + nodeDepths(root.right,depth)
-    
+
+'''
 
 
 # This is the class of the input binary tree.
