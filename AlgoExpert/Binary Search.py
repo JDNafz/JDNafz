550659522ecf97817d3b82f82856f1a388863890
JD <nafziger10@gmail.com>
2022-08-24 17:35:57 -0500

    Update Binary Search.py

--- AlgoExpert/Binary Search.py
+++ AlgoExpert/Binary Search.py
@@ -1,22 +1,33 @@
+'''
+write a binary search method for a given array of integers.
 
+
+https://www.algoexpert.io/questions/binary-search
+'''
+
+'''
+I took a less conventional route to solve this one, I used a multiplyer to divide the total length of the array by 2, 4, 6, etc.
+if target was higher I add 
+'''
 def binarySearch(array, target, i=0):
     # Write your code here.
-
+    if target < array[i] or target > array[-1]:
+        return -1
     multiplyer = 2
-    
     while array[i] != target:
         if array[i] < target and array[i+1] > target:
+            print("-1")
             return -1
         if array[i] < target:
             i = i + len(array)//multiplyer
             multiplyer +=2
-
         else:
             i =i - len(array)//multiplyer
             multiplyer += 2
-    print(i)
+    print("found",array[i]," at idx", i)
     return i
 
-binarySearch([0, 1, 21, 33, 45, 45, 61, 71, 72, 73],33)
-  "array": [1, 5, 23, 111],
-  "target": 35
+
+# binarySearch([0, 1, 21, 33, 45, 45, 61, 71, 72, 73],33)
+# binarySearch([1, 5, 23, 111],35)
+# binarySearch([5, 23, 111],3)
