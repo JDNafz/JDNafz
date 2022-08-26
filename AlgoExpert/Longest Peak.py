c810d88791c263b07c11abb72ac6068cab970371
JD <nafziger10@gmail.com>
2022-08-26 17:34:46 -0500

    Update Longest Peak.py

--- AlgoExpert/Longest Peak.py
+++ AlgoExpert/Longest Peak.py
@@ -1,4 +1,12 @@
 def longestPeak(array):
     # Write your code here.
+    longest = 0
 
     
+
+            
+           
+    print(longest)
+    return longest
+
+longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3])
