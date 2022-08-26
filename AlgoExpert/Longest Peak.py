bc72ef5c82dd34f57bf5c43cea9fd9a5c29841f9
JD <nafziger10@gmail.com>
2022-08-26 18:51:43 -0500

    Update Longest Peak.py

--- AlgoExpert/Longest Peak.py
+++ AlgoExpert/Longest Peak.py
@@ -1,3 +1,6 @@
+from cProfile import label
+
+
 def longestPeak(array):
     # Write your code here.
     longest = 0
@@ -9,27 +12,38 @@ def longestPeak(array):
     decNum = 0
 
     i=0
-    while i < range(len(array-1)):
+    while i < len(array)-1:
+        peak = 0
         if array[i] < array [i+1]:
             inc = True
         while inc == True:
             if array[i] < array[i+1]:
                 incNum =+ 1
+                if dec == True:
+                    labelPeak(peak, firsthalf, decNum)                  
                 i += 1
             elif array[i] > array[i+1]:
-                firsthalf = incNum + 1
+                firsthalf = incNum
+                incNum = 0
+                decNum += 1
                 dec = True
                 i += 1
             else:
                 inc = False
                 incNum = 0
-            if dec == True:
+                if dec == True:
+                    labelPeak(peak,firsthalf,decNum)
+                decNum = 0
+                i+= 1
+    print(longest)
+    return longest
 
-
+def labelPeak(peak, firsthalf, decNum):
+    longest = max(peak,firsthalf + decNum)
+    peak = firsthalf + decNum
 
             
            
-    print(longest)
-    return longest
+
 
 longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3])
