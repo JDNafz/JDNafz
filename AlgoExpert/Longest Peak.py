1988043b8e0add9250b7664b83b09629920d3b76
JD <nafziger10@gmail.com>
2022-08-26 18:42:35 -0500

    Update Longest Peak.py

--- AlgoExpert/Longest Peak.py
+++ AlgoExpert/Longest Peak.py
@@ -1,8 +1,31 @@
 def longestPeak(array):
     # Write your code here.
     longest = 0
+    global inc
+    inc = False
+    global dec
+    dec = False
+    incNum = 0
+    decNum = 0
 
-    
+    i=0
+    while i < range(len(array-1)):
+        if array[i] < array [i+1]:
+            inc = True
+        while inc == True:
+            if array[i] < array[i+1]:
+                incNum =+ 1
+                i += 1
+            elif array[i] > array[i+1]:
+                firsthalf = incNum + 1
+                dec = True
+                i += 1
+            else:
+                inc = False
+                incNum = 0
+            if dec == True:
+
+
 
             
            
