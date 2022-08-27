529620a71e88bccde38ecd0578ced3ec2d6e4307
JD <nafziger10@gmail.com>
2022-08-27 16:46:22 -0500

    Update Longest Peak.py

--- AlgoExpert/Longest Peak.py
+++ AlgoExpert/Longest Peak.py
@@ -1,6 +1,6 @@
 from cProfile import label
 
-
+global longest
 def longestPeak(array):
     # Write your code here.
     longest = 0
@@ -14,28 +14,84 @@ def longestPeak(array):
     i=0
     while i < len(array)-1:
         peak = 0
+        print(array[i])
         if array[i] < array [i+1]:
             inc = True
-        while inc == True:
+            incNum += 1
+            i += 1
+        while inc == True and i < len(array)-1:
+            print(array[i], inc)
+
+
+
             if array[i] < array[i+1]:
+                print("inc")
                 incNum =+ 1
                 if dec == True:
-                    labelPeak(peak, firsthalf, decNum)                  
+                    print("peak^^^")
+                    labelPeak(peak, firsthalf, decNum)                
                 i += 1
+                dec = False
+
+
+
             elif array[i] > array[i+1]:
+                print("dec")
                 firsthalf = incNum
                 incNum = 0
                 decNum += 1
                 dec = True
+                if inc == True:
+                    inc = False
                 i += 1
+
+
             else:
-                inc = False
+                print("same")
                 incNum = 0
                 if dec == True:
+                    print("to the top")
                     labelPeak(peak,firsthalf,decNum)
                 decNum = 0
-                i+= 1
-    print(longest)
+                i += 1
+                inc = False
+                dec = False
+
+
+
+        while dec == True and i < len(array)-1:
+            print(array[i], inc)
+            if array[i] < array[i+1]:
+                print("inc")
+                incNum =+ 1
+                if dec == True:
+                    print("peak^^^")
+                    labelPeak(peak, firsthalf, decNum)                
+                i += 1
+                dec = False
+            elif array[i] > array[i+1]:
+                print("dec")
+                firsthalf = incNum
+                incNum = 0
+                decNum += 1
+                dec = True
+                if inc == True:
+                    inc = False
+                i += 1
+            else:
+                print("same")
+                incNum = 0
+                if dec == True:
+                    print("to the top")
+                    labelPeak(peak,firsthalf,decNum)
+                decNum = 0
+                i += 1
+                inc = False
+                dec = False
+
+
+    print("DONE:",longest)
+    print("peak:",peak)
     return longest
 
 def labelPeak(peak, firsthalf, decNum):
