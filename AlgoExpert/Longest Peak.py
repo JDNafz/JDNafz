970baff7bf6d771d3d40324ea4f1b798a6215de4
JD <nafziger10@gmail.com>
2022-08-29 18:05:38 -0500

    is it working now?

--- AlgoExpert/Longest Peak.py
+++ AlgoExpert/Longest Peak.py
@@ -1,27 +1,55 @@
 
 def longestPeak(array):
     # Write your code here.
-
+    peak = 0
     for i in range(1,len(array)-1):
         if array[i-1] < array[i] > array[i+1]:
-            print("peak found, min array is {},{},{}".format(array[i-1],array[i], array[i+1]))
-            peak = findPeakDistance(array,i)
+            # print("peak found, min array is {},{},{}".format(array[i-1],array[i], array[i+1]))
+            peak = max(findPeakDistance(array,i),peak)
+
         if array[i] < array[i + 1]:
-            print("{} < {}".format(array[i],array[i+1]))
+            # print("{} < {}".format(array[i],array[i+1]))
+            pass
         elif array[i] > array[i +1]:
-            print("{} >{}".format(array[i], array[i+1]))
+            pass
+            # print("{} >{}".format(array[i], array[i+1]))
         else:
-            print("{} == {}".format(array[i],array[i+1]))
+            pass
+            # print("{} == {}".format(array[i],array[i+1]))
+    # print("DONE:",peak)
+    # print(peak)
+    return peak
 
 
 def findPeakDistance(array, idx):
     length = 1
-    for i in reversed(range(idx)):
-        if array[i] > array[i+1]
+    i = idx
+    # print("i < idx")
+    for x in reversed(range(0, idx)):
+        # print(array[x], "<", array[x+1])
+        if array[x] < array[x + 1]:
+            length += 1
+            # print("disney")
+        else:
+            break
+
+    # print("idx > i")
+    for x in range(len(array)- idx-1):
+        # print(array[x+idx], "& ", array[x+idx+1])
+        if array[x+idx] > array[x + 1 + idx]:
+            length += 1
+            # print("mark")
+        else:
+            break
+ 
+    # print("length:",length)
+    return length
 
 
 
 
 
 longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3])
-# longestPeak([1,3,2])
+
+
+# longestPeak([1,3,5,4,2])
