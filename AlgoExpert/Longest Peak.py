ce61e59aa55c4faed737304aa68b62a7f7d8a536
JD <nafziger10@gmail.com>
2022-08-29 16:54:46 -0500

    chaos cointinues on peaks

--- AlgoExpert/Longest Peak.py
+++ AlgoExpert/Longest Peak.py
@@ -1,12 +1,13 @@
-from cProfile import label
-
+global decNum
+global incNum
+global inc
+global dec
 global longest
+
 def longestPeak(array):
     # Write your code here.
     longest = 0
-    global inc
     inc = False
-    global dec
     dec = False
     incNum = 0
     decNum = 0
@@ -14,35 +15,32 @@ def longestPeak(array):
     i=0
     while i < len(array)-1:
         peak = 0
-        print(array[i])
         if array[i] < array [i+1]:
             inc = True
-            incNum += 1
-            i += 1
+        if array[i] > array[i+1]:
+            dec = True
         while inc == True and i < len(array)-1:
-            print(array[i], inc)
-
-
+            print(array[i], "increase while")
 
             if array[i] < array[i+1]:
-                print("inc")
+                print("inc!")
                 incNum =+ 1
                 if dec == True:
-                    print("peak^^^")
-                    labelPeak(peak, firsthalf, decNum)                
+                    print("peak found, {} is a peak! incNum is {}".format(array[i], incNum))
+                    longest - labelPeak(peak, incNum, decNum)                
                 i += 1
                 dec = False
 
-
-
             elif array[i] > array[i+1]:
-                print("dec")
-                firsthalf = incNum
+                print("dec!")
                 incNum = 0
                 decNum += 1
+                if i == len(array)-2:
+                    print("peak found, {} is a peak! incNum is {}".format(array[i], incNum))
+                    longest = labelPeak(peak, incNum, decNum) 
+                    print("peak found, {} is a peak! incNum is {}".format(array[i], incNum))
                 dec = True
-                if inc == True:
-                    inc = False
+                inc = False
                 i += 1
 
 
@@ -56,19 +54,19 @@ def longestPeak(array):
                 i += 1
                 inc = False
                 dec = False
-
-
 
         while dec == True and i < len(array)-1:
-            print(array[i], inc)
+            print(array[i], "decrease while")
             if array[i] < array[i+1]:
                 print("inc")
+                inc = True
                 incNum =+ 1
-                if dec == True:
-                    print("peak^^^")
-                    labelPeak(peak, firsthalf, decNum)                
-                i += 1
+                i+= 1
+                print("peak complete, longest:{}, incNum:{}, decNum{}, array[i]:{}".format(longest, incNum, decNum, array[i]))
+                longest = labelPeak(peak, incNum, decNum)
+                print("peak complete, longest:{}, incNum:{}, decNum{}, array[i]:{}".format(longest, incNum, decNum, array[i]))             
                 dec = False
+
             elif array[i] > array[i+1]:
                 print("dec")
                 firsthalf = incNum
@@ -88,18 +86,25 @@ def longestPeak(array):
                 i += 1
                 inc = False
                 dec = False
-
+        if i == len(array)-2:
+            if dec is True:
+                print("I'm the last num, longest:{}, incNum:{}, decNum{}, array[i]:{}".format(longest, incNum, decNum, array[i]))
+                longest = labelPeak(peak, incNum, decNum)
+                print("I'm the last num, longest:{}, incNum:{}, decNum{}, array[i]:{}".format(longest, incNum, decNum, array[i]))     
 
     print("DONE:",longest)
-    print("peak:",peak)
     return longest
 
-def labelPeak(peak, firsthalf, decNum):
-    longest = max(peak,firsthalf + decNum)
-    peak = firsthalf + decNum
+def labelPeak(peak, incNum, decNum):
+    longest = max(peak, incNum + decNum)
+    peak = incNum + decNum
+    incNum = 0
+    decNum = 0
+    return longest
 
             
            
 
 
-longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3])
+# longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3])
+longestPeak([1,3,2])
