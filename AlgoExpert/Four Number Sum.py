99f792d73a34ca93280df1c56449fd76db08055a
JD <nafziger10@gmail.com>
2022-09-01 13:29:34 -0500

    working on 4numsum

--- AlgoExpert/Four Number Sum.py
+++ AlgoExpert/Four Number Sum.py
@@ -1,10 +1,27 @@
 def fourNumberSum(array, targetSum):
-
     sumDict = {}
-    for i in range(len(array)-1):
-        sum = array[i] + array[i+1]
-        if sum in sumDict:
-            sumDict[sum].append([array[i],array[i+1]])
-        else:
-            sumDict[sum] = [[array[i],array[i+1]]]
+
+    for i in range(len(array)):
+        print("INDEX:",array[i])
+        print("j loop range:", range(i+1,len(array)))
+        for j in range(i+1,len(array)):
+            sum = array[i] + array[j]
+            print("sum",sum)
+            if sum in sumDict:
+                #key is the sum, value is an array of indexes in the main array to add together
+                sumDict[sum].append([i,j])
+            else:
+                sumDict[sum] = [[i,j]]
     print(sumDict)
+    for key in sumDict:
+        
+
+
+
+
+        # print("k loop range:", range(0,i))
+        # for k in range(0,i):
+        #     print("k:{}".format(array[k]))
+
+
+fourNumberSum([7, 6, 4, -1, 1, 2],16)
