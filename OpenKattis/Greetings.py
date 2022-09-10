f7c8cc918a88cc4a7cf52b4fe9c82736b463d222
JD <nafziger10@gmail.com>
2022-09-09 19:09:57 -0500

    deleting open kattis

--- OpenKattis/Greetings.py
+++
@@ -1,9 +0,0 @@
-'''
-Given a string of the form he…ey, print a 
-string containing twice as many e’s.
-https://open.kattis.com/problems/greetings2
-
-🟢 1.3 Easy Difficulty
-'''
-x = input()
-print(x[0],x[1:-1],x[1:-1],x[-1],sep='')
