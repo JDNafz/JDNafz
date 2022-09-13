74d4346c8504426165b36fbfeda37df1047d0948
JD <nafziger10@gmail.com>
2022-09-13 17:58:35 -0500

    Find Successor added. Still working on when to exit.

---
+++ AlgoExpert/Find Successor.py
@@ -0,0 +1,30 @@
+# This is an input class. Do not edit.
+from sqlite3 import complete_statement
+
+
+class BinaryTree:
+    def __init__(self, value, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+
+def findSuccessor(tree, node):
+    global complete 
+    complete = False
+    return inOrderTraverse(tree,node)
+
+
+def inOrderTraverse(tree, node):
+    global complete
+    if complete == True or tree is None:
+        return
+    if tree.left is not None:
+        inOrderTraverse(tree.left,node)
+    # CURR TREE array.append(tree.value)
+    if tree.right is not None:
+        inOrderTraverse(tree.right,node)
+    
+    if tree.value == node
+    return array
