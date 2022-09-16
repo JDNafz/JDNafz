04bc69648c9260ac94b3cef32dfdaad463c4e4c8
JD <nafziger10@gmail.com>
2022-09-16 10:25:55 -0500

    kane's update

--- AlgoExpert/Kadane's Algorithm.py
+++ AlgoExpert/Kadane's Algorithm.py
@@ -10,14 +10,12 @@ def kadanesAlgorithm(array):
     currentSum = 0
     for value in array:
         currentSum += value
-        # print ("({}, {}):{}".format(idx,value,currentSum))
         if currentSum >= largestSum:
-            largestSum = max(largestSum,currentSum)
-            # print("largest is now:",largestSum)
-        if currentSum <= 0:
+            largestSum = currentSum
+        if currentSum < 0:
             currentSum = 0
-    # print("DONE:",largestSum)
     return largestSum
+
 
 # kadanesAlgorithm([0,1,2,3,4,-200,3,4,5,6,-200])
 # kadanesAlgorithm([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4])
