4c4b96dd6efd2afcfb6f1c64ed1f64a102689704
JD <nafziger10@gmail.com>
2022-09-16 19:28:00 -0500

    fixed commit

--- AlgoExpert/Min Number of Coins for Change.py
+++ AlgoExpert/Min Number of Coins for Change.py
@@ -1,2 +1,23 @@
-def minNumberOfCoinsForChange(n, denoms):
-    a
+'''
+
+🔵 Medium
+Needs review, brushed over this too fast.
+'''
+
+def minNumberOfCoinsForChange(n, denoms):    
+    numCoins = [0] + [float("inf") for _ in range(n)]
+    
+    for d in denoms:
+        #amount represents $ amount in the array from 0-n attempting to build towards
+        for amount in range(len(numCoins)):
+            if d <= amount:
+                numCoins[amount] = min(numCoins[amount], numCoins[amount -d] + 1)
+    if numCoins[n] != float("inf"):
+        return numCoins[n]
+    return -1    
+
+
+
+
+
+minNumberOfCoinsForChange(9, [3,5])
