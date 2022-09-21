d9e03a9d6412fb68379b370425afeb8c2538f1cc
JD <nafziger10@gmail.com>
2022-09-21 11:01:19 -0500

    I'm somewhere on river...

--- AlgoExpert/River Sizes.py
+++ AlgoExpert/River Sizes.py
@@ -1,24 +1,69 @@
 def riverSizes(matrix):
-    array = []
-    for row in matrix:
-        for col in matrix:
+    results = []
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
             if matrix[row][col] == 1:
-                riverLength = 1
-                searchRiver(row,col,matrix,array,riverLength)
-    print(array)
-    return array
+                currRiver = 0
+                findRiver(row,col,matrix,results,currRiver)
+    print(results)
+    return results
 
-def searchRiver(row,col,matrix,array,riverLength):
-    riverLength += 1
+def findRiver(row,col,matrix,results,currRiver):
     matrix[row][col] = -1
-
-    if 1 < row < len(matrix[-1]):
-        searchRiver(row+1, col,   matrix,array,riverLength)
-        searchRiver(row-1, col,   matrix,array,riverLength)
-    if 1 < col < len(matrix[1][0]):
-        searchRiver(row,   col+1, matrix,array,riverLength)
-        searchRiver(row,   col-1, matrix,array,riverLength)
-
-    if col == 1 and : 
-        searchRiver(row,col,matrix,array)
+    currRiver +=1
+    if checkRight(row,col,matrix):
+        if matrix[row][col+1] == 1:
+            findRiver(row,col+1,matrix,results,currRiver)
+    if checkLeft(row,col,matrix):
+        if matrix[row][col-1] == 1:
+            findRiver(row,col-1,matrix,results,currRiver)
+    if checkDown(row,col,matrix):
+        if matrix[row+1][col] == 1:
+            findRiver(row+1,col,matrix,results,currRiver)
+    if checkUp(row,col,matrix):
+        if matrix[row-1][col] ==1:
+            findRiver (row-1,col,matrix,results,currRiver)
+    results.append(currRiver)
     return
+
+def checkRight(row,col,matrix):
+    if col != matrix[row][-1]:
+        return True
+    return False
+def checkLeft(row,col,matrix):
+    if col != matrix[row][0]:
+        return True
+    return False
+def checkDown(row,col,matrix):
+    if row != matrix[-1]:
+        return True
+    return False
+def checkUp(row,col,matrix):
+    if row != matrix[0]:
+        return True
+    return False
+
+riverSizes([
+  [1, 0, 0, 1, 0],
+  [1, 0, 1, 0, 0],
+  [0, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1],
+  [1, 0, 1, 1, 0]
+])
+
+
+
+# def searchRiver(row,col,matrix,array,riverLength):
+#     riverLength += 1
+#     matrix[row][col] = -1
+
+#     if 1 < row < len(matrix[-1]):
+#         searchRiver(row+1, col,   matrix,array,riverLength)
+#         searchRiver(row-1, col,   matrix,array,riverLength)
+#     if 1 < col < len(matrix[1][0]):
+#         searchRiver(row,   col+1, matrix,array,riverLength)
+#         searchRiver(row,   col-1, matrix,array,riverLength)
+
+#     if col == 1 and : 
+#         searchRiver(row,col,matrix,array)
+#     return
