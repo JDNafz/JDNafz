c187f6b7196ba06900f80331f2e0bff922527ca5
JD <nafziger10@gmail.com>
2022-09-22 15:20:09 -0500

    headway on islands???

--- AlgoExpert/Remove Islands.py
+++ AlgoExpert/Remove Islands.py
@@ -4,21 +4,64 @@ def removeIslands(matrix):
     
     for row in range(len(matrix)):
         col = len(matrix[0])
-        initialSearch(row,col,matrix,2)
+        if visited[row][col]:
+            continue    
+        initialSearch(row,col,matrix,visited)
+    for row in range(len(matrix)):
         col = 0
-        initialSearch(row,col,matrix,2)
+        if visited[row][col]:
+            continue  
+        initialSearch(row,col,matrix,visited)
     for col in range(len(matrix[0])):
         row = 0
-        initialSearch(row,col,matrix,2)
+        if visited[row][col]:
+            continue 
+        initialSearch(row,col,matrix,visited)
+    for col in range(len(matrix[0])):        
         row = len(matrix)
-        initialSearch(row,col,matrix,2)    
+        col = 0
+        if visited[row][col]:
+            continue 
+        initialSearch(row,col,matrix,visited)    
+
+    printMatrix(matrix)
     return matrix
 
-def initialSearch(row,col,matrix,change):
-    pass
+def initialSearch(row,col,matrix,visited):
+    nodesToCheck = []
+    while len(nodesToCheck) > 0:
+        currentNode = nodesToCheck.pop()
+        row,col = currentNode[0],currentNode[1]
+        if visited[row][col]:
+            continue
+        visited[row][col] = True
+        if matrix[row][col] == 0:
+            continue
+        matrix[row][col] = 2
+        unvisitedNeighbors = getUnvisitedNeighbors(row,col,matrix,visited)
+        for pair in unvisitedNeighbors:
+            nodesToCheck.append(pair)
+
+def getUnvisitedNeighbors(row,col,matrix,visited):
+    pairs = []
+    #up
+    if row > 0 and not visited[ row - 1][col]:
+        pairs.append([row - 1,col])
+    #down
+    if row < len(matrix)-1 and not visited[row + 1][col]:
+        pairs.append([row + 1,col])
+    #left
+    if col > 0 and not visited[row][col-1]:
+        pairs.append([row ,col-1])
+    if col < len(matrix[0])-1 and not visited[row][col+1]:
+        pairs.append([row,col+1])
+    return pairs
 
 
-def printStep(matrix):
+
+
+
+def printMatrix(matrix):
     for i in range(len(matrix)):
         print(matrix[i])
     print("\n")
