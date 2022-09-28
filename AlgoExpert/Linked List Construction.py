7b48e7d7519ad126b5d60a81650f670752481933
JD <nafziger10@gmail.com>
2022-09-28 18:08:53 -0500

    spagetti is cooked linked list

--- AlgoExpert/Linked List Construction.py
+++ AlgoExpert/Linked List Construction.py
@@ -16,24 +16,33 @@ class DoublyLinkedList:
         self.tail = None
 
     def setHead(self, node):
+        if node == self.tail:
+            self.tail = node.prev
+            node.prev.next = None
         if self.head is None:
             self.head = node
             self.tail = node
         else:
-            headNode = self.head
-            headNode.prev = node
-            node.next = headNode
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            self.head.prev = node
+            node.next = self.head
             self.head = node
+        node.prev = None
+        
 
     def setTail(self, node):
+        if node == self.head:
+            self.head = node.next
+            node.next.prev = None
         if self.tail is None:
             self.tail = node
             self.head = node
         else:
-            tailNode = self.tail
-            tailNode.next = node
-            node.prev = tailNode
+            self.tail.next = node
+            node.prev = self.tail
             self.tail = node
+        node.next = None
             
 
     def insertBefore(self, node, nodeToInsert):
@@ -41,7 +50,9 @@ class DoublyLinkedList:
             self.head = nodeToInsert
         else:
             nodeToInsert.prev = node.prev
-        node.prev.next, nodeToInsert.next, node.prev = nodeToInsert, node, nodeToInsert
+            node.prev.next = nodeToInsert
+        node.prev = nodeToInsert
+        nodeToInsert.next = node
 
 
 
@@ -50,25 +61,29 @@ class DoublyLinkedList:
             self.tail = nodeToInsert
         else:
             nodeToInsert.next = node.next 
-        node.next.prev, nodeToInsert.prev, node.next = nodeToInsert, node, nodeToInsert
+            node.next.prev = nodeToInsert
+        node.next = nodeToInsert
+        nodeToInsert.prev = node
 
     def insertAtPosition(self, position, nodeToInsert):
+        if self.head == None:
+            self.head = nodeToInsert
+            self.tail = nodeToInsert
+            return
         node = self.head
         for _ in range(1,position,1):
             node = node.next
-        # print("insertBefore({}, {})".format(node.value,nodeToInsert.value))
         self.insertBefore(node, nodeToInsert)
             
 
     def removeNodesWithValue(self, value):
-        node = self.containsNodeWithValue(value) 
-        while node is not None:
-            self.remove(node)
-            node = self.containsNodeWithValue(value)
+        self.nodeToRemove = []
+        if self.containsNodeWithValue(value):
+            for node in self.nodeToRemove:
+                self.remove(node)
 
         
     def remove(self, node):
-        print("remove({})".format(node.value))
         if node == self.head:
             self.head = node.next
             node.next.prev = None
@@ -84,69 +99,98 @@ class DoublyLinkedList:
         node = self.head
         while node is not None:
             if node.value == value:
-                print("yes contains {}".format(value))
-                return node 
+                self.nodeToRemove.append(node)
             node = node.next
-        return None
+        if self.nodeToRemove != []:
+            return True 
+        return False
 
-    def printHead2Tail(self):
+    def pHeadToTail(self):
         values = []
         nodeNames = []
-        node = linkedList.head
+        node = self.head
         while node is not None:
             values.append(node.value)
             nodeNames.append(node)
             node = node.next
-        print(values)
+        print("H:", values)
         return nodeNames
 
-def printPointers(array):
-    for node in array:
-        if node.prev is not None:
-            prevvar = node.prev.value
-        else:
-            prevvar = "N"
-        if node.next is not None:
-            nextvar = node.next.value
-        else:
-            nextvar = "N"
-        print("    {} \n  /   \ \n {}     {}".format(node.value, prevvar, nextvar))
+    def pTailToHead(self):
+        values = []
+        nodeNames = []
+        node = self.tail
+        while node is not None:
+            values.append(node.value)
+            nodeNames.append(node)
+            node = node.prev
+        values2 = []
+        for i in reversed(values):
+            values2.append(i)
+
+        print("T:", values2)
+        return nodeNames
+
+    def pPointers(self,array):
+        for node in array:
+            if node.prev is not None:
+                prevvar = node.prev.value
+            else:
+                prevvar = "N"
+            if node.next is not None:
+                nextvar = node.next.value
+            else:
+                nextvar = "N"
+            print("    {} \n  /   \ \n {}     {}".format(node.value, prevvar, nextvar))
+    def pBoth(self):
+        self.pHeadToTail()
+        self.pTailToHead()
+
+def t1():
+    linkedList = DoublyLinkedList()
+    one = Node(1)
+    two = Node(2)
+    three = Node(3)
+    three2 = Node(3)
+    three3 = Node(3)
+    four = Node(4)
+    five = Node(5)
+    six = Node(6)
+
+    linkedList.setHead(five)
+    linkedList.setHead(four)
+    linkedList.setHead(three)
+    linkedList.setHead(two)
+    linkedList.setHead(one)
+
+    linkedList.pBoth()
+    linkedList.setHead(four)
+    print(linkedList.head.value)
+    linkedList.pBoth()
+    
+    linkedList.setTail(six)
+
+    
+    linkedList.insertBefore(six,three)
+    linkedList.insertAfter(six,three2)
+
+    linkedList.pBoth()
+
+
+    linkedList.insertAtPosition(1,three3)
+    linkedList.removeNodesWithValue(3)
+    linkedList.remove(two)
+    linkedList.containsNodeWithValue(5)
 
 
 
+def t2():
+    linkedList = DoublyLinkedList()
+    one = Node(1)
+    two = Node(2)
 
-linkedList = DoublyLinkedList()
-one = Node(1)
-two = Node(2)
-three = Node(3)
-three2 = Node(3)
-three3 = Node(3)
-four = Node(4)
-five = Node(5)
-six = Node(6)
-
-linkedList.setHead(five)
-linkedList.setHead(four)
-linkedList.setHead(three)
-linkedList.setHead(two)
-linkedList.setHead(one)
-
-
-linkedList.setHead(four)
-linkedList.setTail(six)
-linkedList.insertBefore(six,three)
-
-head2Tail = linkedList.printHead2Tail()
-
-linkedList.insertAfter(six,three2)
-linkedList.insertAtPosition(1,three3)
-linkedList.remove(two)
-linkedList.containsNodeWithValue(5)
-
-
-
-
-# printPointers(head2Tail)
-# print("Head/Tail",linkedList.head.value, linkedList.tail.value )
-
-head2Tail = linkedList.printHead2Tail()
+    linkedList.setHead(one)
+    linkedList.pHeadToTail()
+    linkedList.insertBefore(one, two)
+    linkedList.pHeadToTail()
+t1()
