d38f74551cdacedf28681662ea0c76b2c8cd151f
JD <nafziger10@gmail.com>
2022-10-06 12:19:12 -0500

    very confused on Permutations

---
+++ AlgoExpert/Permutations.py
@@ -0,0 +1,20 @@
+def getPermutations(array):
+    return permSearch(array,0,[])
+    return array
+
+def permSearch(array,perm, output):
+    if not array:
+        output.append(perm)
+        return
+    for _ in range(len(array)):
+        newArray = array
+        perm = newArray.pop()
+        permSearch(newArray, perm, output)
+    print(array)
+    return array
+
+
+
+getPermutations([1,2,3])
+
+# remove(3, [1,2,3,4])
