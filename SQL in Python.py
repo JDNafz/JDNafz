4e0da0817d69441aaac67e9bcbc6297e5c8e12c3
JD <nafziger10@gmail.com>
2022-10-27 23:04:55 -0500

    SQL course completed, now playing with Jupyter Notebooks

---
+++ SQL in Python.py
@@ -0,0 +1,16 @@
+import import ipykernel
+# In[ ]:
+import psycopg2 as pg2
+conn = pg2.connect(database='dvdrental',user='postgres',password='password')
+cur = conn.cursor()
+cur.execute('SELECT * FROM payment')
+data = cur.fetchmany(2)
+cur.close()
+
+print(data)
+print('dad')
+
+# In[]:
+print('banana')
+
+# %%
