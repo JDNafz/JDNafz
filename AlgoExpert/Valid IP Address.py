44588ae0f950a7d1ac5b5716953cff6e036e6018
JD <nafziger10@gmail.com>
2022-11-01 16:17:19 -0500

    valid IP address

---
+++ AlgoExpert/Valid IP Address.py
@@ -0,0 +1,24 @@
+def validIPAddresses(string):
+    output = []
+    zeroidx = []
+    for i in range(len(string)):
+        if string[i] == '0':
+            zeroidx += i
+    
+    idx = 0
+    while idx <= len(string)
+
+
+
+    return output
+
+def isValidNum(string):
+    for 
+    num = int(string)
+    if 0 <= num <= 255:
+        return True
+    else:
+        return False
+
+beep = "002"
+print(isValidNum(beep))
