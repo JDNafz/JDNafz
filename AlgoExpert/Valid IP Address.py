bfeae1f548d8c4a59888659253551cb944c05201
JD <nafziger10@gmail.com>
2022-11-02 11:50:19 -0500

    IP work

--- AlgoExpert/Valid IP Address.py
+++ AlgoExpert/Valid IP Address.py
@@ -1,24 +1,75 @@
 def validIPAddresses(string):
     output = []
-    zeroidx = []
-    for i in range(len(string)):
-        if string[i] == '0':
-            zeroidx += i
     
-    idx = 0
-    while idx <= len(string)
+    #find first number
+    checklist1 = []
+    startidx = 1
+    endidx = startidx + 3
+    for i in range(1,4):
+        if i > len(string):
+            continue
+        if isValidNum(string[0:i]):
+            checklist1.append(string[0:i])
 
+    #find second number
+    secondnum = []
+    checklist2 = []
+    for i in range(len(checklist1)):
+        numTemp = checklist1[i]
+        startidx = len(numTemp)
+        endidx = startidx + 3
+        for j in range(startidx,endidx):
+            if j > len(string):
+                continue
+            if isValidNum(string[startidx:j]):
+                secondnum.append(string[startidx:j])
 
+        for num in secondnum:
+            checklist2.append(numTemp + "." + num)
+        secondnum = []
+        
+    print("2nd", checklist2)
+
+    #find third number
+    thirdnum = []
+    checklist3 = []
+    for i in range(len(checklist2)):
+        numTemp = checklist2[i]
+        startidx = len(numTemp) - 1 #subtract bc of addding "."
+        endidx = startidx + 3   
+        for j  in range(startidx,endidx):
+            if j > len(string):
+                continue
+            if isValidNum(string[startidx:j]):
+                thirdnum.append(string[startidx:j])
+        
+        for num in thirdnum:
+            checklist3.append(numTemp + "." + num)
+        thirdnum = []
+
+    print("3rd", checklist3)
 
     return output
 
+def checkNextSet(string,checklist):
+    pass
+
+
+
+
+
 def isValidNum(string):
-    for 
+    if len(string) == 0:
+        return False
+    if len(string) > 1:
+        if string[0] == "0":
+            return False
     num = int(string)
-    if 0 <= num <= 255:
+    if 1 <= num <= 255:
         return True
     else:
         return False
 
-beep = "002"
-print(isValidNum(beep))
+beep = "1921680"
+# print(isValidNum(beep))
+validIPAddresses(beep)
