aa0556bd09cd2054cf0a709bb30e3960b450d815
JD <nafziger10@gmail.com>
2022-11-07 11:03:06 -0600

    IP documented and submitted on the website

--- AlgoExpert/Valid IP Address.py
+++ AlgoExpert/Valid IP Address.py
@@ -1,5 +1,12 @@
+'''
+Find all possible valid IP Addresses from a given string of numbers.
+
+🔵 Medium
+https://www.algoexpert.io/questions/valid-ip-addresses 
+'''
+
+
 def validIPAddresses(string):
-    
     #find first number
     checklist = find1stnum(string)
     # plist(checklist)
@@ -11,21 +18,15 @@ def validIPAddresses(string):
 
     #check if length matches original string + 3 periods
     output = []
-
     for ip in checklist:
         if len(ip) == len(string) + 3:
             output.append(ip)
     
-
-    # print("len(output)",len(output))
-    plist(output)
+    # plist(output)
     return output
-
 
 def find1stnum(string):
     checklist1 = []
-    startidx = 1
-    endidx = startidx + 3
     for i in range(1,4):
         if i > len(string):
             continue
@@ -40,19 +41,17 @@ def findNum(string, checklist, periods):
         stringSoFar = checklist[i]
         startidx = len(stringSoFar) - periods
         endidx = startidx + 3
-        # print("stringSoFar: {}, examine string[startidx]:{}".format(stringSoFar,string[startidx]))
 
         for j in range(startidx,endidx):
             if j+1 > len(string):
                 continue
-            # print("jLoop {}.".format(checklist[i]),string[startidx:j+1])
             if isValidNum(string[startidx:j+1]):
                 currChecks.append(string[startidx:j+1])
 
-        print("J Loop complete {}.".format(checklist[i]), currChecks)
+        # print("J Loop complete {}.".format(checklist[i]), currChecks)   # Use to check combinations being added to previous string
         for num in currChecks:
             newChecklist.append(stringSoFar + "." + num)
-        currChecks = [] #clears currChecks before going to next checklist[i]
+        currChecks = []
     return newChecklist
 
 def isValidNum(string):
@@ -67,51 +66,7 @@ def isValidNum(string):
     else:
         return False
 
-def plist(list):
-    for ip in list:
-        print(ip)
 
+#Running a test:
 beep = "1921680"
-# print(isValidNum(beep))
 validIPAddresses(beep)
-
-""" 
-    #find second number
-    secondnum = []
-    checklist2 = []
-    for i in range(len(checklist1)):
-        numTemp = checklist1[i]
-        startidx = len(numTemp)
-        endidx = startidx + 3
-        for j in range(startidx,endidx):
-            if j > len(string):
-                continue
-            if isValidNum(string[startidx:j]):
-                secondnum.append(string[startidx:j])
-
-        for num in secondnum:
-            checklist2.append(numTemp + "." + num)
-        secondnum = []
-        
-    print("2nd", checklist2)
-
-    #find third number
-    thirdnum = []
-    checklist3 = []
-    for i in range(len(checklist2)):
-        numTemp = checklist2[i]
-        startidx = len(numTemp) - 1 #subtract bc of addding "."
-        endidx = startidx + 3   
-        for j  in range(startidx,endidx):
-            if j > len(string):
-                continue
-            if isValidNum(string[startidx:j]):
-                thirdnum.append(string[startidx:j])
-        
-        for num in thirdnum:
-            checklist3.append(numTemp + "." + num)
-        thirdnum = []
-
-    print("3rd", checklist3)
-
-    return output"""
