a2eb60a33ffd46c06a3f2e9a4ca7a51f0ef19859
JD <nafziger10@gmail.com>
2022-11-10 15:04:21 -0600

    4 number sum fixed

--- AlgoExpert/Hard Problems/Four Number Sum.py
+++ AlgoExpert/Hard Problems/Four Number Sum.py
@@ -1,20 +1,28 @@
+'''
+Find all sets of combinations that  4 of which sum to the target
+
+🔴 Hard
+https://www.algoexpert.io/questions/four-number-sum
+'''
+
 def fourNumberSum(array, targetSum):
     sumDict = {}
     ret = []
     for i in range(len(array)):
-        print("INDEX:",array[i])
-        print("j loop range:", range(i+1,len(array)))
-        for j in range(i+1,len(array)):
+        # print("INDEX:",array[i])
+        # print("j loop range:", range(i+1,len(array)))
+        for j in range(i + 1, len(array)):
             sum = array[i] + array[j]
-            print("sum",sum)
-            if sum in sumDict:
-                #key is the sum, value is an array of indexes in the main array to add together
-                sumDict[sum].append([i,j])
+            difference = targetSum - sum
+            if difference in sumDict:
+                for pair in sumDict[difference]:
+                    ret.append(pair + [array[i], array[j]])
+        for k in range(0, i):
+            sum = array[i] + array[k]
+            if sum not in sumDict:
+                sumDict[sum] = [[array[k],array[i]]]
             else:
-                sumDict[sum] = [[i,j]]
-            currentMissingSum = targetSum - (array[i] + array[j])
-            if currentMissingSum in sumDict:
-                pass
+                sumDict[sum].append([array[k], array[i]])
     print(ret)
     return ret
 
