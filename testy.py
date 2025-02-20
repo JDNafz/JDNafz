3873b446a00344feab6aceb6336c02655d518fc2
JDNafz <nafziger10@gmail.com>
2025-02-19 18:52:34 -0600

    added notes to python test file

--- testy.py
+++ testy.py
@@ -1,7 +1,12 @@
+# execute a pythong file in the 
+# terminal with the command: python testy.py
 fruit = "Raspberries"
 fav_fruit = fruit
 fruit = "Blackberries"
 fav_fruit = fruit
 print(fav_fruit)
 
-print(1+2+"3")
+
+# this print fails because you 
+# can't add a string to an integer
+# print(1+2+"3")
